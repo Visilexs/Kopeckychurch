fdb6ffe4069ed50b22ab8dcd821d336513194d79
Visilexs <48158701+Visilexs@users.noreply.github.com>
2026-05-31 10:41:39 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import KopeckyChurch from './kopecky_church.jsx'
+import KopeckyChurch from '/kopecky_church.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
