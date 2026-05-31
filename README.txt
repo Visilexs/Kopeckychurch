bd210816b3da90e6d15d077c9e82cecc1f3fb27a
Visilexs <48158701+Visilexs@users.noreply.github.com>
2026-05-31 10:37:31 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+Cloudflare Pages settings:
+
+Framework preset: Vite
+Build command: npm run build
+Build output directory: dist
+Root directory: (leave blank)
+
+Upload this project, not the previous ZIP.
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KopeckyChurch from './kopecky_church.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KopeckyChurch />
+  </React.StrictMode>
+)
